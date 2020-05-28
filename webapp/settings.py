05c8d39644a8e0a2ef9bad2cab9a8b04199ba2a0
decoda-555 <decoda555@gmail.com>
2020-05-28 15:43:02 -0700

    django complete with default sqlite3

--- webapp/settings.py
+++ webapp/settings.py
@@ -76,20 +76,22 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD': 'blog',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'blog',
+#        'USER': 'blog',
+#        'PASSWORD': 'blog',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
