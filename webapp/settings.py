992e49009f143e928c3c0814448e909fb92cd203
Chris White <cwhite@nai-online.com>
2020-05-28 12:50:03 -0700

    cw

--- webapp/settings.py
+++ webapp/settings.py
@@ -113,3 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'decoda555@gmail.com'
+EMAIL_HOST_PASSWORD = 'Xz02y324'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
