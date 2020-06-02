33e887f831659350a2de2b57d23bf60609720777
decoda-555 <66085135+decoda-555@users.noreply.github.com>
2020-06-02 13:37:32 -0700

    Update testPosts.py

--- testPosts.py
+++ testPosts.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class PostTestCase(TestCase):
     def testPost(self):
-        post = Post(title="My Title", description="Blurb", wiki="Post Body")
-        self.assertEqual(post.title, "My Title")
-        self.assertEqual(post.description, "Blurb")
-        self.assertEqual(post.wiki, "Post Body")
+    post = Post(title='Another post',
+                 slug='another-post',
+                 body='Post body.',
+                 author=user)
